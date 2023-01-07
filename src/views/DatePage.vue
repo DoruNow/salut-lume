67c84dae015cd62e7c61a1635dbb5ca5c3b622fc
doru <doru.dev.now@gmail.com>
2023-01-07 14:11:10 +0100

    prepare for first group

---
+++ src/views/DatePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>ye {{ date }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: null,
+    };
+  },
+  created() {
+    this.axios.get().then((d) => {
+      this.date = new Date(d.data);
+    });
+  },
+};
+</script>
