3e5e144af4900fe7f7dc4258788123dd681b0a44
Mihai Popeanga <doru@DESKTOP-92BSIG0>
2023-01-21 08:27:39 +0100

    add modern text inputs

--- src/components/demo/components/DemoInputs.vue
+++ src/components/demo/components/DemoInputs.vue
@@ -2,21 +2,35 @@
   <v-card>
     <v-tabs v-model="tab" fixed-tabs elevation="10">
       <v-tab dark>Basic Inputs</v-tab>
+      <v-tab dark>Modern Text Inputs</v-tab>
+      <v-tab dark>Other Inputs</v-tab>
     </v-tabs>
 
     <v-tabs-items v-model="tab">
       <v-tab-item>
         <BasicInputs />
       </v-tab-item>
+
+      <v-tab-item>
+        <ModernInputs />
+      </v-tab-item>
+
+      <v-tab-item>
+        <OtherInputs />
+      </v-tab-item>
     </v-tabs-items>
   </v-card>
 </template>
 <script>
 import BasicInputs from "../inputs/BasicInputs.vue";
+import ModernInputs from "../inputs/ModernInputs.vue";
+import OtherInputs from "../inputs/OtherInputs.vue";
 
 export default {
   components: {
     BasicInputs,
+    ModernInputs,
+    OtherInputs,
   },
   data() {
     return {
---
+++ src/components/demo/inputs/ModernInputs.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-card :class="cardClass">
+      <v-card-text class="demo-card-simple-text">
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="Regular"></v-text-field>
+            <v-text-field label="Place" placeholder="Placeholder"></v-text-field>
+            <v-text-field label="Counter" placeholder="Placeholder" counter></v-text-field>
+            <v-text-field label="Max 10 char" placeholder="Placeholder" counter maxlength="10"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center">
+            <CodeEditor :languages="[['HTML', 'HTML']]" :read_only="true" :value="valueHTMLOne" class="codeEditor" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+
+    <v-card :class="cardClass">
+      <v-card-text class="demo-card-simple-text">
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="Outlined" outlined></v-text-field>
+            <v-text-field label="Dense" dense outlined></v-text-field>
+            <v-text-field label="Your product or service" value="Grocery delivery" hint="For example, flowers or used cars"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center">
+            <CodeEditor :languages="[['HTML', 'HTML']]" :read_only="true" :value="valueHTMLTwo" class="codeEditor" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+
+    <v-card :class="cardClass">
+      <v-card-text class="demo-card-simple-text">
+        <v-row>
+          <v-col cols="12">
+            <v-text-field solo label="Prepend" prepend-icon="mdi-map-marker"></v-text-field>
+            <v-text-field solo label="Prepend inner" prepend-inner-icon="mdi-map-marker"></v-text-field>
+            <v-text-field solo label="Append" append-icon="mdi-map-marker"></v-text-field>
+            <v-text-field solo label="Append outer" append-outer-icon="mdi-map-marker"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center">
+            <CodeEditor :languages="[['HTML', 'HTML']]" :read_only="true" :value="valueHTMLThree" class="codeEditor" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import CodeEditor from "simple-code-editor";
+export default {
+  components: {
+    CodeEditor,
+  },
+  data() {
+    return {
+      valueHTMLOne: `<v-text-field label="Regular"></v-text-field>
+<v-text-field label="Place" placeholder="Placeholder"></v-text-field>
+<v-text-field label="Counter" placeholder="Placeholder"></v-text-field>
+<v-text-field label="Max 10 char" placeholder="Placeholder" counter maxlength="10"></v-text-field>`,
+      valueHTMLTwo: `<v-text-field label="Outlined" outlined></v-text-field>
+<v-text-field label="Dense" dense outlined></v-text-field>
+<v-text-field label="Your product or service" value="Grocery delivery" hint="For example, flowers or used cars"></v-text-field>`,
+      valueHTMLThree: `<v-text-field solo label="Prepend" prepend-icon="mdi-map-marker"></v-text-field>
+<v-text-field solo label="Prepend inner" prepend-inner-icon="mdi-map-marker"></v-text-field>
+<v-text-field solo label="Append" append-icon="mdi-map-marker"></v-text-field>
+<v-text-field solo label="Append outer" append-outer-icon="mdi-map-marker"></v-text-field>`,
+    };
+  },
+  computed: {
+    demoItem() {
+      return `d-flex flex-column align-center justify-center demo-card-text`;
+    },
+    cardClass() {
+      return `ma-4 elevation-10`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.demo-card-simple-text {
+  width: 100%;
+  background-color: rgb(156, 209, 255);
+}
+</style>
